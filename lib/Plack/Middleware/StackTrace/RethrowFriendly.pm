8e50710b48300e3a585f57769cf878b7a06b8666
tarao <tarao.gnn@gmail.com>
2015-03-12 20:00:05 +0900

    About the other errors.

--- lib/Plack/Middleware/StackTrace/RethrowFriendly.pm
+++ lib/Plack/Middleware/StackTrace/RethrowFriendly.pm
@@ -210,6 +210,10 @@ that if you catch (C<eval> or C<try>-C<catch> for example) an error
 and rethrow (C<die> or C<croak> for example) it, the original stack
 trace not the rethrown one is displayed.
 
+When the response is displayed as an HTML, all the errors including
+rethrown ones are visible through the throwing point selector at the
+top of the HTML.
+
 =head1 SEE ALSO
 
 L<Plack::Middleware::StackTrace>
